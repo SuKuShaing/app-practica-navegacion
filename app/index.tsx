cff69d68295723333029ba11cba5bb23eccf8216
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-23 17:01:18 -0400

    Se instala nativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 const App = () => {
 	return (
 		<View>
-			<Text>Hola mundo</Text>
+			<Text className="mt-10 text-red-500 text-2xl font-bold">Hola seba</Text>
 		</View>
 	);
 };
