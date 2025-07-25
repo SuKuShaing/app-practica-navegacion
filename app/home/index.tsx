a4b341a32a4898b2fbd748bbe4b6d1a317419e21
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-25 13:43:58 -0400

    Se añade ForwardRef para recibir la funcionalidad del padre y se añaden variantes del botón

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,12 +6,20 @@ import { SafeAreaView, View } from "react-native";
 const HomeScreen = () => {
 	return (
 		<SafeAreaView>
-			<View className="px-10 mt-8 gap-5">
-				<Link href="/profile" asChild>
+			<View className="px-10 mt-8 gap-4">
+				<Link href="/profile" asChild> 
+					{/* La prop asChild le dice al componente Link que no renderice su propio elemento DOM/componente, hace que el CustomButton herede la funcionalidad de navegación del Link padre. */}
 					<CustomButton color="secondary">Profile</CustomButton>
 				</Link>
-				<CustomButton color="primary" onPress={() => router.push("/productos")}>
+				
+				<CustomButton 
+				className="mb-10"
+				color="primary" onPress={() => router.push("/productos")}>
 					Productos
+				</CustomButton>
+
+				<CustomButton variant="text-only" onPress={() => router.push("/settings")}>
+					Settings
 				</CustomButton>
 
 				{/* <Link className="mt-5" href="/productos">Productos{' '}</Link>
--- components/shared/CustomButton.tsx
+++ components/shared/CustomButton.tsx
@@ -1,32 +1,63 @@
-import React from "react";
-import { Pressable, PressableProps, Text } from "react-native";
+import { forwardRef, Ref } from "react";
+import { Pressable, PressableProps, Text, View } from "react-native";
 
 interface Props extends PressableProps {
 	children: string;
 	color?: "primary" | "secondary" | "tertiary";
+	variant?: "contained" | "text-only";
+	className?: string;
 }
 
-const CustomButton = ({
-	children,
-	color = "primary",
-	onPress,
-	onLongPress,
-}: Props) => {
-	const btnColor = {
-		primary: "bg-primary",
-		secondary: "bg-secondary",
-		tertiary: "bg-tertiary",
-	}[color];
+// forwardRef es una función de React que permite que un componente funcional (componente funcional son funciones de Js/Ts que retornan elementos JSX) reciba y pase hacia adelante una referencia (ref) a un elemento hijo
+// ref es una referencia a un elemento del DOM, es una forma de acceder a los elementos del DOM desde el componente padre
+const CustomButton = forwardRef(
+	(
+		{
+			children,
+			color = "primary",
+			onPress,
+			onLongPress,
+			variant = "contained",
+			className,
+		}: Props,
+		ref: Ref<View>
+	) => {
+		const btnColor = {
+			primary: "bg-primary",
+			secondary: "bg-secondary",
+			tertiary: "bg-tertiary",
+		}[color];
+		
+		const textColor = {
+			primary: "text-primary",
+			secondary: "text-secondary",
+			tertiary: "text-tertiary",
+		}[color];
 
-	return (
-		<Pressable
-			className={`p-3 rounded-md ${btnColor} active:opacity-80`}
-			onPress={onPress}
-			onLongPress={onLongPress}
-		>
-			<Text className="text-white text-center">{children}</Text>
-		</Pressable>
-	);
-};
+		if (variant === "text-only") {
+			return (
+				<Pressable
+					className={`p-3 ${className}`}
+					onPress={onPress}
+					onLongPress={onLongPress}
+					ref={ref}
+				>
+					<Text className={`text-center ${textColor} font-work-medium`}>{children}</Text>
+				</Pressable>
+			);
+		}
+
+		return (
+			<Pressable
+				className={`p-3 rounded-md ${btnColor} active:opacity-80 ${className}`}
+				onPress={onPress}
+				onLongPress={onLongPress}
+				ref={ref}
+			>
+				<Text className={`text-white text-center font-work-medium`}>{children}</Text>
+			</Pressable>
+		);
+	}
+);
 
 export default CustomButton;
