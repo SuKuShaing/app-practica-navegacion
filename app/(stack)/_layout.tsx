a95e34c36939a1741c57df924203e33b10af8fda
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-25 16:04:07 -0400

    personalización del stackNavigation

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -3,11 +3,37 @@ import React from "react";
 
 const StackLayout = () => {
 	return (
-		<Stack>
+		<Stack
+			screenOptions={
+				{
+					// headerShown: false
+					//  headerStyle: {
+					// 	backgroundColor: "blue",
+					//  },
+					//  headerTintColor: "white",
+					//  headerTitleStyle: {
+					// 	fontFamily: "WorkSans-Medium",
+					//  },
+					headerShadowVisible: false,
+					contentStyle: {
+						backgroundColor: "white",
+					}
+				}
+			}
+		>
 			<Stack.Screen name="home/index" options={{ title: "Home Screen" }} />
-			<Stack.Screen name="profile/index" options={{ title: "Profile Screen" }} />
-			<Stack.Screen name="productos/index" options={{ title: "Productos Screen" }} />
-			<Stack.Screen name="settings/index" options={{ title: "Settings Screen" }} />
+			<Stack.Screen
+				name="profile/index"
+				options={{ title: "Profile Screen" }}
+			/>
+			<Stack.Screen
+				name="productos/index"
+				options={{ title: "Productos Screen" }}
+			/>
+			<Stack.Screen
+				name="settings/index"
+				options={{ title: "Settings Screen" }}
+			/>
 		</Stack>
 	);
 };
